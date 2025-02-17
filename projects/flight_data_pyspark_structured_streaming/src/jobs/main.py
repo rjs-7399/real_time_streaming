4da7187920e3801c261d6fc07592633adb340228
rutvikshah <rutvikjshah@gmail.com>
2025-02-17 12:02:28 +0530

    Added spark session builder

--- projects/flight_data_pyspark_structured_streaming/src/jobs/main.py
+++ projects/flight_data_pyspark_structured_streaming/src/jobs/main.py
@@ -8,6 +8,17 @@ ANOMALIES_TOPIC = "transaction_anomalies"
 CHECKPOINT_DIR = "/mnt/spark-checkpoints"
 STATE_DIR = "/mnt/spark-state"
 
+spark = (SparkSession.builder
+         .appName('FinancialTransactionProcessor')
+         .config('spark.jars.packages', 'org.apache.spark:spark-sql-kafka-0-10_2.13:3.5.0')
+         .config('spark.sql.streaming.stateStore.stateStoreDir', STATE_DIR)
+         .config('spark.sql.shuffle.partitions', 20)
+         ).getOrCreate()
+
+spark.sparkContext.setLogLevel("WARN")
+
+
+
 def main():
     pass
 
