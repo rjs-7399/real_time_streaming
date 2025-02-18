2b2d221badf08330dc6cc87e70a76e5df876d46b
rutvikshah <rutvikjshah@gmail.com>
2025-02-18 12:59:27 +0530

    Modified docker compose file to add the jar files initially and not at the run time

--- projects/flight_data_pyspark_structured_streaming/src/jobs/main.py
+++ projects/flight_data_pyspark_structured_streaming/src/jobs/main.py
@@ -1,8 +1,8 @@
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import StructType, from_json, col, sum, cast, to_json, struct
+from pyspark.sql.functions import StructType, from_json, col, sum, cast, to_json, struct, count
 from pyspark.sql.types import StringType, StructField, DoubleType, LongType
 
-KAFKA_BROKERS = "localhost:29092,localhost:39092,localhost:49092"
+KAFKA_BROKERS = "kafka-broker-1:19092,kafka-broker-2:19092,kafka-broker-3:19092"
 SOURCE_TOPIC = "financial_transactions"
 AGGREGATES_TOPIC = "transaction_aggregates"
 ANOMALIES_TOPIC = "transaction_anomalies"
@@ -17,6 +17,7 @@ spark = (SparkSession.builder
          ).getOrCreate()
 
 spark.sparkContext.setLogLevel("WARN")
+
 
 transaction_schema = StructType([
     StructField('transactionId', StringType(), True),
@@ -36,6 +37,7 @@ kafka_stream = (spark.readStream
                 .option("kafka.bootstrap.servers", KAFKA_BROKERS)
                 .option("subscribe", SOURCE_TOPIC)
                 .option("startingOffsets", "earliest")
+                .option("failOnDataLoss", "false")
                 ).load()
 
 transaction_df = kafka_stream.selectExpr("CAST(value AS STRING)") \
@@ -48,6 +50,7 @@ transaction_df = transaction_df.withColumn('transactionTimestamp',
 aggregated_df = transaction_df.groupBy("merchantId")\
     .agg(
     sum("amount").alias('totalAmount'),
+    count("*").alias("transactionCount")
 )
 
 aggregation_query = (
